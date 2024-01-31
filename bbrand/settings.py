e883688162132985af6bfc75d9f404ceda794296
Rayhan Billah <devilzhut@gmail.com>
2024-01-31 16:50:00 +0600

    added allowed host

--- bbrand/settings.py
+++ bbrand/settings.py
@@ -26,7 +26,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 CORS_ORIGIN_WHITELIST = [
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'rawlife.pythonanywhere.com',
 ]
 # Application definition
 
